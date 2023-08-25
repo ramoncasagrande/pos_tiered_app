1ccf3ba93c0991cb2021fa7990fe501e7a69de69
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-08-25 20:20:02 -0300

    Adiciondo fetch para conectar a camada de negocio

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,27 @@
+import React, { useState, useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+  const [greeting, setGreeting] = useState([]);
+
+async function fetchGreeting() {
+  const response = await fetch("http://localhost:3001/");
+  setGreeting(await response.json());
+}
+
+useEffect(() => {
+  fetchGreeting();
+}, [greeting]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello React!
+           {greeting} + React!
         </p>
       </header>
     </div>
